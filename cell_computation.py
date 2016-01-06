18d8a7b71a08a4adf7715d07ebe6c1bd013c21b9
Yi Hu <hyhuyiharry@gmail.com>
2016-01-06 01:15:59 +0100

    3 Method explanation added, add self.comp_state label, add material_post to improve performance

--- cell_computation.py
+++ cell_computation.py
@@ -3,6 +3,7 @@
 from dolfin import *
 import numpy as np
 from cell_geom import PeriodicBoundary_no_corner
+from cell_material import Material
 
 parameters["form_compiler"]["cpp_optimize"] = True
 ffc_options = {"optimize": True,
@@ -16,10 +17,9 @@ class MicroComputation(object):
     unit cell computation for 2D multi field problem
     !! 3D should be extended !!
     """
-
     def __init__(self, cell, material_li,
                  strain_gen_li, strain_FS_li,
-                 multi_field_label):
+                 multi_field_flag):
         """
         Initialize cell properties such as geometry, material, boundary,
         post processing space, and switch of multi field
@@ -28,9 +28,9 @@ class MicroComputation(object):
         :param bc: boundary conditions
         :param material_li: [mat1, mat2, ...]
         :param strain_FS_li: post processing spaces
-        :param multi_field_label: 1: multi, 0: uni
+        :param multi_field_flag: 1: multi, 0: uni
 
-        ** Attention: multi_field_label should be set currently, when uni
+        ** Attention: multi_field_flag should be set currently, when uni
         field, assign in merge method does not work **
         """
         self.cell = cell
@@ -46,6 +46,8 @@ class MicroComputation(object):
         self.F_w = None
         self.J = None
 
+        self.comp_state = None
+
         # __Helping class member__
         # Geometry Dimension 2D
         self.field_num = None
@@ -58,11 +60,14 @@ class MicroComputation(object):
         self.w_split = None
         # Boundary Condition
         self.bc = None
-        self.w_dim = None
-        self.multi_field_label = multi_field_label
+        self.multi_field_flag = multi_field_flag
         self.material_num = len(material_li)
 
         # F for Post Processing
+        self.material_post = [Material(mat_i.energy_func, mat_i.para_list,
+                                       mat_i.invar_gen_li.keys(),
+                                       mat_i.invar_gen_li.values())
+                              for mat_i in material_li]
         self.F_merge = None
         self.F_merge_test = None
         self.F_merge_trial = None
@@ -81,21 +86,22 @@ class MicroComputation(object):
         assert isinstance(F_bar_li, list) and isinstance(w_li, list)
         self._F_bar_init(F_bar_li)
         self.w = w_li
-        self.w_dim = [w_i.shape() for w_i in w_li]
         self.field_num = len(w_li)
-        label = 'w'
-        self._field_merge(label)
-        self._field_split(label)
-        self._strain_init(label)
+
+        self.comp_state = 'pre'
+        self._field_merge(self.comp_state)
+        self._field_split(self.comp_state)
+        self._strain_init(self.comp_state)
 
     def _F_bar_init(self, F_bar_li):
         """
         Macro field input, initialize F_bar
         Mind that if multiple field, multiple field input will require
 
-        :param F_bar_li: !! only constant macro field input are support !!
+        :param F_bar_li: !! only constant macro field input are supported !!
         F_bar_li should be a list of F_bar, each entry is furthermore a list
         representing input from each field
+
         :return self.F_bar: [Function for F, Function for M, Function for T,...]
         """
         assert isinstance(F_bar_li[0], list)
@@ -123,9 +129,9 @@ class MicroComputation(object):
         else:
             raise Exception('Please Input Right Dimension')
 
-    def _field_merge(self, label):
-        if label is 'w':
-            if self.multi_field_label:
+    def _field_merge(self, state):
+        if state is 'pre':
+            if self.multi_field_flag:
                 FS_li = [wi.function_space() for wi in self.w]
                 MFS = MixedFunctionSpace(FS_li)
                 self.w_merge = Function(MFS)
@@ -139,8 +145,8 @@ class MicroComputation(object):
                 self.v_merge = TestFunction(FS)
                 self.dw_merge = TrialFunction(FS)
                 assign(self.w_merge, self.w[0])
-        elif label is 'F':
-            if self.multi_field_label:
+        elif state is 'post':
+            if self.multi_field_flag:
                 FS_li = [Fi.function_space() for Fi in self.F]
                 MFS = MixedFunctionSpace(FS_li)
                 self.F_merge = Function(MFS)
@@ -155,49 +161,49 @@ class MicroComputation(object):
                 self.F_merge_trial = TrialFunction(FS)
                 assign(self.F_merge, self.F[0])
         else:
-            raise Exception('wrong label for merge!')
+            raise Exception('wrong state for merge!')
 
-    def _field_split(self, label):
-        if label is 'w':
-            if self.multi_field_label:
+    def _field_split(self, state):
+        if state is 'pre':
+            if self.multi_field_flag:
                 self.w_split = list(split(self.w_merge))
             else:
                 self.w_split = [self.w_merge]
-        elif label is 'F':
-            if self.multi_field_label:
+        elif state is 'post':
+            if self.multi_field_flag:
                 self.F_split = list(split(self.F_merge))
             else:
                 self.F_split = [self.F_merge]
         else:
-            raise Exception('wrong label for split!')
+            raise Exception('wrong state for split!')
 
-    def _strain_init(self, label):
+    def _strain_init(self, state):
         generator_li = self.strain_gen
-        if label is 'w':
+        if state is 'pre':
             self.F = [gen(self.F_bar[i], self.w_split[i])
                       for i, gen in enumerate(generator_li)]
-        elif label is 'F':
+        elif state is 'post':
             self.F = [self.F_bar[i] + self.F_split[i]
                       for i in range(self.field_num)]
         else:
-            raise Exception('wrong label for strain init!')
+            raise Exception('wrong state for strain init!')
 
     # ==== Pre-Processing Stage ====
-    def _material_assem(self, F):
+    def _material_assem(self, F, material_list):
         for i in range(self.material_num):
-            self.material[i](F)
+            material_list[i](F)
 
         # print id(self.material[0].invar[0])
 
-    def _total_energy(self, F):
+    def _total_energy(self, F, material_list):
         # Please be careful about the ordering dx(0) -> matrix
         dx = Measure('dx', domain=self.cell.mesh,
                      subdomain_data=self.cell.domain)
 
-        self._material_assem(F)
+        self._material_assem(F, material_list)
 
         int_i_li = [mat.psi*dx(i)
-                    for i, mat in enumerate(self.material)]
+                    for i, mat in enumerate(material_list)]
         self.Pi = sum(int_i_li)
 
     def _bc_fixed_corner(self):
@@ -214,7 +220,7 @@ class MicroComputation(object):
         self.bc = bc
 
     def _fem_formulation_composite(self):
-        self._total_energy(self.F)
+        self._total_energy(self.F, self.material)
 
         F_w = derivative(self.Pi, self.w_merge, self.v_merge)
 
@@ -238,61 +244,44 @@ class MicroComputation(object):
 
     # ==== Post-Processing Stage ====
     def _energy_update(self):
-        label = 'F'
         self.comp_strain()
-        self._field_merge(label)
-        self._field_split(label)
-        self._strain_init(label)
-        # self._material_assem(self.F_split)
-        self._total_energy(self.F_split)
+        self._field_merge(self.comp_state)
+        self._field_split(self.comp_state)
+        self._strain_init(self.comp_state)
+        self._total_energy(self.F_split, self.material_post)
         # plot(self.F_merge[0,0], interactive=True)
         # plot(self.F[0][0,0], interactive=True)
 
     def comp_strain(self):
         F_space = self.strain_FS
-        self._strain_init('w')
         self.F = [project(self.F[i], F_space_i)
                   for i, F_space_i in enumerate(F_space)]
+
+        self.comp_state = 'post'
 
         # plot(self.F[0][0,0], interactive=True)
 
         print 'strain computation finished'
 
     def comp_stress(self):
-        # TODO can have a try on both "local diff and global assem" and
-        # "global diff and global assem", comparison with derivative is also
-        # worthful
         if not self.F_merge:
             self._energy_update()
 
-        self.P = Function(self.F_merge.function_space())
+        P = Function(self.F_merge.function_space())
+
         L = derivative(self.Pi, self.F_merge, self.F_merge_test)
-        # Pi_der = diff(self.Pi, self.F_merge)
-        # self.P = project(Pi_der, self.F_merge.function_space())
 
         dx = Measure('dx', domain=self.cell.mesh,
                      subdomain_data=self.cell.domain)
-
-        # L = inner(Pi_der, self.F_merge_test)
         a = inner(self.F_merge_test, self.F_merge_trial)
-        #
-        # int_L_li = [L*dx(i) for i in range(self.material_num)]
         int_a_li = [a*dx(i) for i in range(self.material_num)]
-        #
-        # L = sum(int_L_li)
         a = sum(int_a_li)
-        # self.P = project(Pi_der, self.F_merge.function_space())
-        solve(a == L, self.P)
-        # solve(L == 0, self.P, self.bc, J=a,
-        #       form_compiler_parameters=ffc_options)
 
-        # Pi_der = derivative(Pi, self.F[0], F_test)
-        # Pi_der = diff(Pi, self.F[0])
-        # self.P = project(Pi_der, F_space[0])
+        solve(a == L, P)
 
         print 'stress computation finished'
 
-        # plot(self.P[0,0], interactive=True)
+        plot(P[0,0], interactive=True)
 
     def avg_merge_strain(self):
         if not self.F_merge:
@@ -303,7 +292,7 @@ class MicroComputation(object):
         d = self.geom_dim
         mat_num = self.material_num
 
-        if self.multi_field_label:
+        if self.multi_field_flag:
             F_merge_dim = self.F_merge.shape()[0]
             F_merge_avg = np.zeros((F_merge_dim, 1))
             for i in range(F_merge_dim):
@@ -326,37 +315,58 @@ class MicroComputation(object):
         if not self.F_merge:
             self._energy_update()
 
-        dx = Measure('dx', domain=self.cell.mesh,
-                     subdomain_data=self.cell.domain)
-        d = self.geom_dim
-        mat_num = self.material_num
+        # 1. Method: direct make derivative to global energy and use Constant
+        # Function as TestFunction. Result is a Vector. No use of mat_num
+        # explicitly
+        TFS_R = TensorFunctionSpace(self.cell.mesh, 'R', 0)
+        F_const_test = TestFunction(TFS_R)
 
-        P_i = range(mat_num)
+        L = derivative(self.Pi, self.F_merge, F_const_test)
 
-        for i in range(mat_num):
-            P_i[i] = diff(self.material[i].psi, self.F_merge)
-            # P_i[i] = diff(self.material[i].psi, self.F_split[0])
-            # P_i[i] = diff(self.material[i].psi, self.F_bar[0])
-            # P_i[i] = derivative(self.material[i].psi, self.F_merge, self.F_merge_test)
+        P_merge_avg = assemble(L).array()
 
+        # 2. Method: make derivative to local energy and integrate then
+        # assemble, mind that also use Constant Function as TestFunction.
+        # Result is a Vector. derivative() seems to stack the DoF into a column!
+        # dx = Measure('dx', domain=self.cell.mesh,
+        #              subdomain_data=self.cell.domain)
+        # mat_num = self.material_num
+        #
+        # P_i = range(mat_num)
+        #
+        # TFS_R = TensorFunctionSpace(self.cell.mesh, 'R', 0)
+        # F_const_test = TestFunction(TFS_R)
+        # for i in range(mat_num):
+        #     P_i[i] = derivative(self.material[i].psi, self.F_merge, F_const_test)
+        #
         # int_li = [P_i[k]*dx(k) for k in range(mat_num)]
-        # P_merge_avg = assemble(sum(int_li))
+        # P_merge_avg = assemble(sum(int_li)).array()
 
-        if self.multi_field_label:
-            F_merge_dim = self.F_merge.shape()[0]
-            P_merge_avg = np.zeros((F_merge_dim, 1))
-            for i in range(F_merge_dim):
-                int_li = [P_i[k][i]*dx(k) for k in range(mat_num)]
-                P_merge_avg[i, 0] = assemble(sum(int_li))
-        else:
-            P_merge_avg = np.zeros((d, d))
-            for i in range(d):
-                for j in range(d):
-                    int_li = [P_i[k][i, j]*dx(k) for k in range(mat_num)]
-                    P_merge_avg[i, j] = assemble(sum(int_li))
-
-        # Pi_der = derivative(self.Pi, self.F_split[0], FF_bar_trial)
-        # P_merge_avg = assemble(Pi_der)
+        # 3. Method: make diff to local energy and integrate finally
+        # assemble. Without using Constant TestFunction. Result maybe a
+        # tensor. diff() conserve the tensor structure. The reason may due to
+        # the integration
+        # dx = Measure('dx', domain=self.cell.mesh,
+        #              subdomain_data=self.cell.domain)
+        # d = self.geom_dim
+        # mat_num = self.material_num
+        #
+        # P_i = range(mat_num)
+        # for i in range(mat_num):
+        #     P_i[i] = diff(self.material[i].psi, self.F_merge)
+        #
+        # if self.multi_field_flag:
+        #     F_merge_dim = self.F_merge.shape()[0]
+        #     P_merge_avg = np.zeros((F_merge_dim, 1))
+        #     for i in range(F_merge_dim):
+        #         int_li = [P_i[k][i]*dx(k) for k in range(mat_num)]
+        #         P_merge_avg[i, 0] = assemble(sum(int_li))
+        # else:
+        #     P_merge_avg = np.zeros((d, d))
+        #     for i in range(d):
+        #         for j in range(d):
+        #             int_li = [P_i[k][i, j]*dx(k) for k in range(mat_num)]
+        #             P_merge_avg[i, j] = assemble(sum(int_li))
 
         print 'average merge stress computation finished'
 
@@ -388,35 +398,6 @@ class MicroComputation(object):
 
         C_avg = self.avg_merge_moduli()
 
-        # Old 2nd method more detailed and explicit calculation: diff locally
-        #  and integrate
-        # FF = self.F_merge
-        # psi_m_ = self.material[0].psi
-        # psi_i_ = self.material[1].psi
-        #
-        # # calculate the local moduli
-        # P_i = diff(psi_i_, FF)
-        # P_m = diff(psi_m_, FF)
-        #
-        # C_i_ = diff(P_i, FF)
-        # C_m_ = diff(P_m, FF)
-        #
-        # # trial and test function for the L matrix
-        # TFS_R = TensorFunctionSpace(self.cell.mesh, 'R', 0)
-        # FF_bar_trial = TrialFunction(TFS_R)
-        # FF_bar_test = TestFunction(TFS_R)
-        #
-        # # generate the constant form of C and assemble C
-        # dx = Measure('dx', domain=self.cell.mesh,
-        #              subdomain_data=self.cell.domain)
-        # i, j, k, l = indices(4)
-        # c = FF_bar_test[i, j]*C_i_[i, j, k, l]*FF_bar_trial[k, l]*dx(1) + \
-        #     FF_bar_test[i, j]*C_m_[i, j, k, l]*FF_bar_trial[k, l]*dx(0)
-        # cc = assemble(c)
-        # CC = cc.array()
-
-        print C_avg
-
         TFS_R = TensorFunctionSpace(self.cell.mesh, 'R', 0)
         F_bar_trial = TrialFunction(TFS_R)
         L2 = derivative(self.F_w, self.F_bar, F_bar_trial)
@@ -424,7 +405,7 @@ class MicroComputation(object):
 
         LTKL2 = self.sensitivity(B2)
 
-        print LTKL2
+        # print LTKL2
 
         print C_avg - LTKL2
         return C_avg - LTKL2
@@ -455,13 +436,10 @@ class MicroComputation(object):
         B.apply('insert')
         L = B.array()
 
-        # use the matrix from 1st method
         LTKL = np.zeros((F_merge_len, F_merge_len))
-
         L_assign = Function(self.w_merge.function_space())
         x = Vector()
 
-        # compute four columns
         for i in range(F_merge_len):
             L_assign.vector().set_local(L[:, i])
             solve(K_a, x, L_assign.vector())
@@ -469,7 +447,7 @@ class MicroComputation(object):
         return LTKL
 
 
-def cauchy_green_with_macro(F_bar, w_component):
+def deform_grad_with_macro(F_bar, w_component):
     return F_bar + grad(w_component)
 
 if __name__ == '__main__':
@@ -498,7 +476,7 @@ if __name__ == '__main__':
     F_bar = [0.9, 0., 0., 1.]
     w = Function(VFS)
     strain_space = TensorFunctionSpace(mesh, 'DG', 0)
-    comp = MicroComputation(cell, mat_li, [cauchy_green_with_macro],
+    comp = MicroComputation(cell, mat_li, [deform_grad_with_macro],
                             [strain_space], 0)
 
     comp.input([F_bar], [w])
@@ -507,7 +485,7 @@ if __name__ == '__main__':
     # Post-Processing
     # comp._energy_update()
     # comp.comp_strain()
-    comp.comp_stress()
+    # comp.comp_stress()
     # comp.avg_merge_strain()
     # comp.avg_merge_stress()
     comp.effective_moduli_2()
--- cell_material.py
+++ cell_material.py
@@ -2,21 +2,29 @@
 
 from dolfin import Identity, tr
 
+
 class Material(object):
     """
     Material Class for 2D problem
     possible to extend it into 3D
     """
-    def __init__(self, energy_func, para_list):
+
+    def __init__(self, energy_func, para_list, field_label_tuple_list=None,
+                 invariant_generator_list_list=None):
         self.energy_func = energy_func
-        self.para_li = para_list
+        self.para_list = para_list
 
         self.F = None
         self.num_field = 0
         self.field_dim = []
         self.prob_dim = 0
 
-        self.invar_gen_li = {}
+        if not field_label_tuple_list:
+            self.invar_gen_li = {}
+        else:
+            self.invar_gen_li = dict(zip(field_label_tuple_list,
+                                         invariant_generator_list_list))
+
         self.invar = []
         self.psi = None
 
@@ -28,22 +36,28 @@ class Material(object):
 
         :return: complete instance
         """
-        self.func_init(func_list)
-        self.material_energy()
-
-    # Initialize instance separately, Modularize material assemble.
-    def func_init(self, func_list):
         if not isinstance(func_list, list):
             raise Exception('please input a list')
         self.F = func_list
-        self.num_field = len(func_list)
-        self.prob_dim = func_list[0].geometric_dimension()
+        self._material_energy()
 
-    def invariant_generator_init(self, field_label_tuple,
-                                 invariant_generator_list):
+    def invariant_generator_append(self, field_label_tuple,
+                                   invariant_generator_list):
+        """
+        Please be careful! When the keys are the same, it will override the
+        initial one!
+
+        :param field_label_tuple: field dependency, e.g. (0,), (0,1)
+        :param invariant_generator_list: function list on the field, e.g. tr
+
+        :return: renewed or updated invar_gen_li
+        """
         self.invar_gen_li[field_label_tuple] = invariant_generator_list
 
     def _invariant(self):
+        if not self.invar_gen_li:
+            raise Exception('Please initialize the invariant generator list '
+                            'first')
         invariant_generator = self.invar_gen_li
         self.invar = []
         for fields_label in invariant_generator:
@@ -51,14 +65,32 @@ class Material(object):
                 fields = [self.F[i] for i in fields_label]
                 self.invar.append(generator(*fields))
 
-    def material_energy(self):
+    def _material_energy(self):
         """
         Subroutine that builds material energy (psi)
 
         :return: self.psi
         """
         self._invariant()
-        self.psi = self.energy_func(self.invar, *self.para_li)
+        self.psi = self.energy_func(self.invar, *self.para_list)
+
+    def direction(self):
+        """
+        :ivar
+        :return:
+        """
+        pass
+
+    def __str__(self):
+        if not self.num_field:
+            self._problem_setting()
+        return '%dD Material depends on %d fields' % (self.prob_dim,
+                                                      self.num_field)
+
+    def _problem_setting(self):
+        func_list = self.F
+        self.num_field = len(func_list)
+        self.prob_dim = func_list[0].geometric_dimension()
 
     def get_field_dim(self):
         for i, field in enumerate(self.F):
@@ -72,22 +104,9 @@ class Material(object):
             else:
                 print 'vector field: %d' % field_shape[0]
 
-    def comp_general_strain(self):
-        pass
-
-    def comp_general_stress(self):
-        pass
-
-    def mech_energy(self):
-        pass
-
-    def material_update(self):
-        pass
-
-    def direction(self):
-        pass
-
     def green_lagrange(self, F):
+        if not self.prob_dim:
+            self._problem_setting()
         I = Identity(self.prob_dim)
         C = F.T * F
         E = 0.5 * (C - I)
@@ -112,18 +131,19 @@ class Material(object):
         return generator_list
 
     @staticmethod
-    def compose(f, g):
-        return [lambda x: fi(g(x)) for fi in f]
+    def compose(f_list, g):
+        return [lambda x: fi(g(x)) for fi in f_list]
 
 
 def st_venant_kirchhoff(E_m, nu_m):
     mu = E_m / (2 * (1 + nu_m))
     lmbda = E_m * nu_m / ((1 + nu_m) * (1 - 2 * nu_m))
+
     # mu = Constant(E_m / (2 * (1 + nu_m)))
     # lmbda = Constant(E_m * nu_m / ((1 + nu_m) * (1 - 2 * nu_m)))
 
     def psi(inv, lmbda, mu):
-        return 0.5*lmbda*(inv[0])**2 + mu*inv[1]
+        return 0.5 * lmbda * (inv[0]) ** 2 + mu * inv[1]
 
     svk = Material(psi, [lmbda, mu])
 
@@ -141,10 +161,10 @@ def st_venant_kirchhoff(E_m, nu_m):
 
     # Method 1: Use compose to generate invar_func
     # gen_li = svk.compose(svk.tensor_invar((1,2)),svk.green_lagrange)
-    # svk.invariant_generator_init((0,), gen_li)
+    # svk.invariant_generator_append((0,), gen_li)
 
     # Method 2: Direct generate using functions
-    svk.invariant_generator_init((0,), [invariant1, invariant2])
+    svk.invariant_generator_append((0,), [invariant1, invariant2])
 
     return svk
 
@@ -162,19 +182,22 @@ def simo_pister(mu0, m0, lmbda0, ro0, cv, theta0):
         :param cv: thermal capacity
         :param theta0: initial temperature
         """
-        return 0.5*mu0*(inva[0]-3) + (m0*(inva[2]-theta0) - mu0) * ln(inva[1]) +\
-               0.5*lmbda0*ln(inva[1])**2 -\
-               ro0*cv*(inva[2]*ln(inva[2]/theta0) - (inva[2]-theta0))
+        return 0.5 * mu0 * (inva[0] - 3) + (m0 * (inva[2] - theta0) - mu0) * ln(
+                inva[1]) + \
+               0.5 * lmbda0 * ln(inva[1]) ** 2 - \
+               ro0 * cv * (inva[2] * ln(inva[2] / theta0) - (inva[2] - theta0))
 
     sp = Material(psi, [mu0, m0, lmbda0, ro0, cv, theta0])
 
     C_invar_gen = sp.compose(sp.tensor_invar((1,)), sp.green_lagrange)
+
     def invariant3(C):
         return det(C) ** 0.5
+
     C_invar_gen.append(invariant3)
 
-    sp.invariant_generator_init((0,), C_invar_gen)
-    sp.invariant_generator_init((1,), [lambda x: x, ])
+    sp.invariant_generator_append((0,), C_invar_gen)
+    sp.invariant_generator_append((1,), [lambda x: x, ])
 
     return sp
 
@@ -191,16 +214,18 @@ def magneto_mechano(N, a, b, c):
     :param b: coeff
     :param c: coeff
     """
+
     def psi(inv, a, b, c):
         return a * (inv[0] + inv[1] + inv[2]) + b * (inv[3] + inv[4]) + c * inv[
             5]
-    mre = Material(psi,[a,b,c])
+
+    mre = Material(psi, [a, b, c])
 
     C_invar_gen = mre.tensor_invar((1, 2, 3))
     C_invar_gen.append(lambda x: inner(N, x * N))
-    mre.invariant_generator_init((0,), C_invar_gen)
-    mre.invariant_generator_init((1,), [lambda x: inner(x, x), ])
-    mre.invariant_generator_init((0, 1), [lambda x, y: inner(y, x * y), ])
+    mre.invariant_generator_append((0,), C_invar_gen)
+    mre.invariant_generator_append((1,), [lambda x: inner(x, x), ])
+    mre.invariant_generator_append((0, 1), [lambda x, y: inner(y, x * y), ])
 
     return mre
 
@@ -222,18 +247,32 @@ if __name__ == '__main__':
 
     E_m, nu_m = 10.0, 0.3
     svk = st_venant_kirchhoff(E_m, nu_m)
+    svk2 = st_venant_kirchhoff(E_m, nu_m)
+
+    print id(svk)
+    print id(svk2)
+
     svk([F])
+    mesh2 = UnitSquareMesh(1, 1)
+    VFS2 = VectorFunctionSpace(mesh2, 'CG', 2)
+    w2 = Function(VFS2)
+    F2 = grad(w2)
+    svk2([F2])
+
+    print id(svk)
+    print id(svk2)
 
     mu0, m0, lmbda0, ro0, cv, theta0 = 1, 2, 3, 4, 5, 6
     theta = Function(FS)
     sp = simo_pister(mu0, m0, lmbda0, ro0, cv, theta0)
-    sp([F.T*F, theta,])
+    sp([F.T * F, theta, ])
 
     C = Function(TFS)
     M = Function(VFS)
     N = Constant((1., 1.))
     a, b, c = 1., 2., 3.
     mre = magneto_mechano(N, a, b, c)
-    mre([C, M,])
+    mre([C, M, ])
 
-    print mre.psi
+    # print mre
+    # print mre.psi
